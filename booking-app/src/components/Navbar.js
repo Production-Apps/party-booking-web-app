fdab1fae056d08d1f386f1d73a6921ae21317402
fritzgt <tuanis@live.com>
2020-09-10 20:23:15 +0300

    made changes to nav bar

--- booking-app/src/components/Navbar.js
+++ booking-app/src/components/Navbar.js
@@ -14,45 +14,104 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Slide from '@material-ui/core/Slide';
+
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import Home from '@material-ui/icons/Home';
 import PersonPin from '@material-ui/icons/PersonPin';
-import Store from '@material-ui/icons/Store';
+import Storefront from '@material-ui/icons/Storefront';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
-const Navbar = () => {
-  const [value, setValue] = React.useState(2);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
 
   return (
-    <Paper>
-      <Tabs
-        value={value}
-        indicatorColor="primary"
-        textColor="primary"
-        onChange={handleChange}
-      >
-        <Tab label="Home" icon={<Home />} component={Link} to="/" />
-        <Tab label="Store" icon={<Store />} component={Link} to="/store" />
-        <Tab label="" disabled />
-        <Tab label="" disabled />
-        <Tab label="" disabled />
-        <Tab label="Login" icon={<PersonPin />} component={Link} to="/login" />
-        <Tab
-          label="Cart"
-          icon={<ShoppingCartIcon />}
-          component={Link}
-          to="/cart"
-        />
-      </Tabs>
-    </Paper>
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
   );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
 };
 
-export default Navbar;
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar>
+          {/* <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <IconButton color="inherit" component={Link} to="/">
+            <Home />
+          </IconButton>
+          <IconButton color="inherit" component={Link} to="/store">
+            <Storefront />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <IconButton color="inherit" component={Link} to="/cart">
+            <ShoppingCartIcon />
+          </IconButton>
+
+          <IconButton color="inherit" component={Link} to="/login">
+            <PersonPin />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
