c111f4938fd6a1ac6266236a01f0eef74ea7079a
fritzgt <tuanis@live.com>
2020-09-10 16:28:18 +0300

    added material-ui

--- booking-app/src/App.js
+++ booking-app/src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+
+//Navigation bar
+import Navbar from './components/Navbar';
+//Home page
+import Home from './components/Home';
+//Authentication
+import Login from './components/Login';
+//Store page
+import Store from './components/Store';
+
+//Admin dashboard *Secure*
+
+//user dashboard *Secure*
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/store" component={Store} />
       </header>
     </div>
   );
